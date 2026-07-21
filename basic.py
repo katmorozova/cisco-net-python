f67056ad4484f7bb9d091eecdbac03c7157611a8
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2026-07-21 23:04:47 +0200

    primer punto del ejercicio realizado en basic.py

--- basic.py
+++ basic.py
@@ -5,3 +5,32 @@ print("Ekaterina")
 print("La Witsi Witsi Araña\nsubió a su telaraña.")
 print()
 print("Vino la lluvia\ny se la llevó.")
+
+#el argumento de palabra clave end determina los caracteres que la función print() envía a la salida
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+# sep como separador ->  usa un guión, en lugar de un espacio, para separar los argumentos de salida
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+#Ambos argumentos de palabra clave pueden mezclarse en una invocación
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+#Salida esperada: Programming***Essentials***in...Python
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#minimizar el número de invocaciones de la función print() insertando \n en las cadenas
+print("    *   \n   * *  \n  *   * ")
+print(" *     * \n***   ***")
+print("  *   *\n  *   *\n  *****")
