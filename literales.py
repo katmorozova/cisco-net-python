a46c54e584a604e412078ada28ee44b458a7116b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2026-07-21 23:55:01 +0200

    ejercicio realizado en literales.py

---
+++ literales.py
@@ -0,0 +1,13 @@
+#insertar un apóstrofe en una cadena la cual está limitada por dos apóstrofes
+print('I\'m Monty Python.')
+print('I\'m Monty Python.')
+
+
+#SALIDA ESPERADA:
+#"Estoy"
+#""aprendiendo""
+#"""Python"""
+
+print("\"Estoy\"")
+print("\"\"aprendiendo\"\"")
+print("\"\"\"Python\"\"\"")
