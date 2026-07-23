72804797ad4ae0cc37271154a47f3bf8f17aa28a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2026-07-23 23:02:24 +0200

    Realizado ejercicio de Entradas y salidas en input.py

--- input.py
+++ input.py
@@ -36,12 +36,32 @@ print("+" + 10 * "-" + "+")
 print(("|" + " " * 10 + "|\n")*5, end="")
 print("+" + 10 * "-" + "+")
 
+# función str() 
+leg_a = float(input("Ingresa la longitud del primer cateto: "))
+leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+print("La longitud de la hipotenusa es:" + str((leg_a**2 + leg_b**2) ** .5))
 
+"""
+Tu tarea es completar el código para evaluar los resultados de cuatro operaciones aritméticas básicas.
+Los resultados deben imprimirse en la consola.
+    # ingresa un valor flotante para la variable a aquí
+    # ingresa un valor flotante para la variable b aquí
+    
+    # mostrar el resultado de la suma aquí
+    # mostrar el resultado de la resta aquí
+    # mostrar el resultado de la multiplicación aquí
+    # mostrar el resultado de la división aquí
+"""
 
+var_a = float(input("Ingresa un valor flotante para la variable a aquí: "))
+var_b = float(input("Ingresa un valor flotante para la variable b aquí: "))
 
+print("El resultado de la suma es:", var_a + var_b)
+print("El resultado de la resta es:", var_a - var_b)
+print("El resultado del multiplicación es:", var_a * var_b)
+print("El resultado de la división es:", var_a / var_b)
 
-
-
+print("\n¡Eso es todo, amigos!")
 
 
 
