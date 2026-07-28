94dd123a80933ea156c85b348e21a34ae95ae296
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2026-07-28 22:52:36 +0200

    Cambios en condiciones.py añadidos

--- condiciones.py
+++ condiciones.py
@@ -74,5 +74,29 @@ if number3 > largest_number:
 # Imprime el resultado.
 print("El número más grande es:", largest_number)
 
+"""
+Espatifilo, más comúnmente conocida como la planta de Cuna de Moisés o flor de la paz, es una de las plantas para interiores más populares que filtra las toxinas dañinas del aire. Algunas de las toxinas que neutraliza incluyen benceno, formaldehído y amoníaco.
+
+Imagina que tu programa de computadora ama estas plantas. Cada vez que recibe una entrada en forma de la palabra Espatifilo, grite involuntariamente a la consola la siguiente cadena: "¡Espatifilo es la mejor planta de todas!"
+
+Escribe un programa que utilice el concepto de ejecución condicional, tome una cadena como entrada y que:
+
+    imprima el enunciado "Si - ¡El Espatifilo! es la mejor planta de todos los tiempos!" en la pantalla si la cadena ingresada es "ESPATIFILIO" (mayúsculas)
+    imprima "No, ¡quiero un gran Espatifilo!" si la cadena ingresada es "espatifilo" (minúsculas)
+    imprima "¡Espatifilo!, ¡No [entrada]!" de lo contrario. Nota: [entrada] es la cadena que se toma como entrada.
+
+Prueba tu código con los datos que te proporcionamos. ¡Y hazte de un Espatifilo también!
+"""
+name = input("Introduce el nombre de la flor: ")
+
+if name == "ESPATIFILIO":
+    print("Si, ¡El ESPATIFILIO es la mejor planta de todos los tiempos!")
+elif name == "espatifilo":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print("¡ESPATIFILIO!, ¡No", name + "!")
+
+    
+
 
 
